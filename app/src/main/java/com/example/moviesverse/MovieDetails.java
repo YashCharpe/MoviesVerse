20531399090b9fe086395dd50c3cce76c7971ca9
Yash Charpe <yashrcharpe07@gmail.com>
2022-07-02 16:33:45 +0530

    Developed Movie Details UI

--- app/src/main/java/com/example/moviesverse/MovieDetails.java
+++ app/src/main/java/com/example/moviesverse/MovieDetails.java
@@ -3,14 +3,21 @@ package com.example.moviesverse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MovieDetails extends AppCompatActivity {
+
+    private ImageView backdropImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        backdropImageView = findViewById(R.id.backdropImageView);
+
+        backdropImageView.setClipToOutline(true);
 
         Bundle bundle = getIntent().getExtras();
 
